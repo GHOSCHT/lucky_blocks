1608d0051257698b0e4ad9b6de2ddd6e30ff3658
GHOSCHT <31184695+GHOSCHT@users.noreply.github.com>
2021-12-28 00:15:01 +0100

    Make blocks placeable

--- src/main/java/com/ghoscht/luckyblocks/LuckyBlocks.java
+++ src/main/java/com/ghoscht/luckyblocks/LuckyBlocks.java
@@ -2,15 +2,13 @@ package com.ghoscht.luckyblocks;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(LuckyBlocks.MODID)
 public class LuckyBlocks {
     public static final String MODID = "luckyblocks";
-    private static final Logger LOGGER = LogManager.getLogger();
 
     public LuckyBlocks() {
+        RegistryHandler.init();
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
--- src/main/java/com/ghoscht/luckyblocks/RegistryHandler.java
+++ src/main/java/com/ghoscht/luckyblocks/RegistryHandler.java
@@ -13,18 +13,20 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class RegistryHandler {
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
-            LuckyBlocks.MODID);
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
-            LuckyBlocks.MODID);
+        public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
+                        LuckyBlocks.MODID);
+        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+                        LuckyBlocks.MODID);
 
-    public static void init() {
-        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
-    }
+        public static void init() {
+                ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+                BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        }
 
-    static Block lbb = new Block(BlockBehaviour.Properties.of(Material.DIRT).sound(SoundType.AMETHYST));
-    public static final RegistryObject<Block> LUCKY_BLOCK_BASIC = BLOCKS.register("lucky_block_basic", () -> lbb);
+        public static final RegistryObject<Block> LUCKY_BLOCK_BASIC = BLOCKS.register("lucky_block_basic",
+                        () -> new Block(BlockBehaviour.Properties.of(Material.DIRT).sound(SoundType.AMETHYST)));
 
-    public static final RegistryObject<Item> LUCKY_BLOCK_BASIC_ITEM = ITEMS.register("lucky_block_basic",
-            () -> new BlockItem(lbb, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+        public static final RegistryObject<Item> LUCKY_BLOCK_BASIC_ITEM = ITEMS.register("lucky_block_basic",
+                        () -> new BlockItem(LUCKY_BLOCK_BASIC.get(),
+                                        new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 }
